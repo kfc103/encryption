86870fcb82ba86ec24d595d86f4269c5ee01a6eb
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-06-30 11:46:54 +0800

    test read all

--- functions/encrypt-read.js
+++ functions/encrypt-read.js
@@ -9,15 +9,24 @@ exports.handler = (event, context) => {
     secret: process.env.FAUNADB_SERVER_SECRET
   });
   const id = getId(event.path);
-  console.log(`Function 'todo-read' invoked. Read id: ${id}`);
+  console.log(`Function 'encrpyt-read' invoked. Read id: ${id}`);
   return client
-    .query(q.Get(q.Ref(`classes/todos/${id}`)))
+    .query(q.Paginate(q.Match(q.Ref("indexes/all_passwords"))))
     .then((response) => {
-      console.log("success", response);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      };
+      const todoRefs = response.data;
+      console.log("Todo refs", todoRefs);
+      console.log(`${todoRefs.length} todos found`);
+      // create new query out of todo refs. http://bit.ly/2LG3MLg
+      const getAllTodoDataQuery = todoRefs.map((ref) => {
+        return q.Get(ref);
+      });
+      // then query the refs
+      return client.query(getAllTodoDataQuery).then((ret) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(ret)
+        };
+      });
     })
     .catch((error) => {
       console.log("error", error);
