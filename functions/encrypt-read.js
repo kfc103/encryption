9a9366f5729544d9a784789061383ad389e14316
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-07-02 14:41:52 +0800

    test read function

--- functions/encrypt-read.js
+++ functions/encrypt-read.js
@@ -10,32 +10,29 @@ exports.handler = (event, context) => {
   });
   const id = getId(event.path);
   console.log(`Function 'encrpyt-read' invoked. Read id: ${id}`);
-  return (
-    client
-      //.query(q.Paginate(q.Match(q.Index("pwd_by_user_id"), id))
-      .query(q.Paginate(q.Events(q.Match(q.Index("pwd_by_user_id"), 1))))
-      .then((response) => {
-        const refs = response.data;
-        console.log("Todo refs", refs);
-        console.log(`${refs.length} todos found`);
-        // create new query out of todo refs. http://bit.ly/2LG3MLg
-        const getAllTodoDataQuery = refs.map((ref) => {
-          return q.Get(ref);
-        });
-        // then query the refs
-        return client.query(getAllTodoDataQuery).then((ret) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify(ret)
-          };
-        });
-      })
-      .catch((error) => {
-        console.log("error", error);
+  return client
+    .query(q.Paginate(q.Match(q.Index("pwd_by_user_id"), 1)))
+    .then((response) => {
+      const refs = response.data;
+      console.log("Todo refs", refs);
+      console.log(`${refs.length} todos found`);
+      // create new query out of todo refs. http://bit.ly/2LG3MLg
+      const getAllTodoDataQuery = refs.map((ref) => {
+        return q.Get(ref);
+      });
+      // then query the refs
+      return client.query(getAllTodoDataQuery).then((ret) => {
         return {
-          statusCode: 400,
-          body: JSON.stringify(error)
+          statusCode: 200,
+          body: JSON.stringify(ret)
         };
-      })
-  );
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      };
+    });
 };
