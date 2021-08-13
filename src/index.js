73bc9e49ce1bc254af7ac546728454bbc97cb0b1
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-08-13 14:59:59 +0800

    test netlifyIdentity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import netlifyIdentity from "netlify-identity-widget";
+
+window.netlifyIdentity = netlifyIdentity;
+// You must run this once before trying to interact with the widget
+netlifyIdentity.init();
 
 const rootElement = document.getElementById("root");
 
