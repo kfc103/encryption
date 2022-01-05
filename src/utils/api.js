9d79fd25b2dcdd768e99349e96f54337f2d91996
Aidan C <74046191+kfc103@users.noreply.github.com>
2022-01-05 18:05:13 +0000

    test

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,8 @@ const readAll = (user) => {
     //"https://goofy-elion-2b3cba.netlify.app/.netlify/functions/encrypt-read-all"
     "https://esecret.netlify.app/.netlify/functions/encrypt-read-all"
   ).then((response) => {
-    console.log(response.json());
+    const r = response.json();
+    console.log(r.result);
     return response.json();
   });
 
