b9a56ca8be244be400ec8f55a5fb87110f5870cb
Aidan C <74046191+kfc103@users.noreply.github.com>
2022-01-07 14:12:22 +0000

    insert function

--- functions/encrypt-insert.js
+++ functions/encrypt-insert.js
@@ -1,3 +1,7 @@
+/* Import faunaDB sdk */
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -9,12 +13,35 @@ exports.handler = async (event, context) => {
 
   console.log(event.body);
   console.log(JSON.parse(event.body));
-
   const params = JSON.parse(event.body);
-  const name = params.name || "World";
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+  });
+  console.log(`Function 'encrpyt-insert' invoked.`);
+  return client
+    .query(
+      q.Create(q.Collection("passwords"), {
+        data: {
+          user_id: "44753dc6-4e9a-4581-8d25-ffee04d61539",
+          name: "Facebook1",
+          login: "1fb.test@test.com",
+          password: "U2FsdGVkX1/ILkcmUOX6hQcS97/dpmRpKNw3c0jFUbw="
+        }
+      })
+    )
+    .then((ret) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(ret)
+      };
+    })
+    .catch((error) => {
+      console.log("error", error);
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      };
+    });
 };
