dedcb0e4d0bace0d576d36610f2d0dd4fe755974
Aidan C <74046191+kfc103@users.noreply.github.com>
2022-01-07 11:38:29 +0000

    update insert function

--- functions/encrypt-insert.js
+++ functions/encrypt-insert.js
@@ -8,6 +8,10 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
+
+  console.log(event.body);
+  console.log(JSON.parse(event.body));
+
   const params = querystring.parse(event.body);
   const name = params.name || "World";
 
